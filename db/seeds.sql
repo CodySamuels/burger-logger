84d05f27c267adf3947edd6a0903841b8c3d6c15
Cody Samuels <twowoodenspoons@gmail.com>
2020-07-17 15:11:28 -0700

    Set up the schema and seeds for the database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,3 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burger (burger_name) VALUES ('Big Mac');
+INSERT INTO burger (burger_name) VALUES ('Impossible Whopper');
+INSERT INTO burger (burger_name, sleepy) VALUES ('Veggie Burger', true);
